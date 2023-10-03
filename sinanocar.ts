214ed373de616e043d7b0285169225649c78ef36
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-10-04 00:42:58 +0900

    更新 sinanocar.ts

--- sinanocar.ts
+++ sinanocar.ts
@@ -66,6 +66,17 @@ led.enable(false)
 pins.setEvents(DigitalPin.P6, PinEventType.Pulse)
 pins.setEvents(DigitalPin.P7, PinEventType.Pulse)
 
+let noservo = 0
+led.enable(false)
+let color_value = 0
+let volt = 0
+
+
+let neo_sinamon = neopixel.create(DigitalPin.P9, 2, NeoPixelMode.RGB)
+neo_sinamon.setBrightness(15)
+
+
+
 //% color="#ff4500" weight=94 
 namespace sinamon {
 
@@ -96,6 +107,61 @@ namespace sinamon {
         //% block="backward",
         backward
     }
+
+    let volt = pins.analogReadPin(AnalogPin.P1) / 500 * 6
+
+    export enum kyori {
+        //% block="long"
+        long,
+        //% block="short",
+        short
+    }
+
+
+
+    export enum onoff {
+        //% block="ON"
+        ON,
+        //% block="OFF"
+        OFF
+    }
+    export enum whiteblack {
+        //% block="black"
+        black,
+        //% block="white"
+        white
+    }
+
+
+
+
+    export enum colorcycle {
+        //% block="cycle1"
+        cycle1,
+        //% block="cycle10",
+        cycle10,
+        //% block="cycle42",
+        cycle42,
+        //% block="cylce64",
+        cycle64,
+        //% block="cycle256",
+        cycle256
+    }
+
+    export enum colorgain {
+        //% block="1×gain"
+        gain1,
+        //% block="4×gain",
+        gain4,
+        //% block="16×gain",
+        gain16,
+        //% block="60×gain",
+        gain60
+    }
+
+
+
+
 
 
     pins.onPulsed(DigitalPin.P6, PulseValue.High, function () {
@@ -410,34 +476,410 @@ function ステップ右回転(数値: number) {
 }
 
 
-// 制御周期分だけ待つ処理（ｍｓ）
-T = 0.01
-pins.setPull(DigitalPin.P6, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P7, PinPullMode.PullNone)
-led.enable(false)
-pins.setEvents(DigitalPin.P6, PinEventType.Pulse)
-pins.setEvents(DigitalPin.P7, PinEventType.Pulse)
 
 //music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-basic.forever(function () {
-    if (走行モード == 1) {
-        if (左カウンター < 右カウンター) {
-            pins.analogWritePin(AnalogPin.P2, 1023)
-            pins.analogWritePin(AnalogPin.P16, 450)
-        } else {
-            pins.analogWritePin(AnalogPin.P2, 450)
-            pins.analogWritePin(AnalogPin.P16, 1023)
+
+    //% color="#009A00" weight=22 blockId=sonar_ping_2 block="Distance sensor" group="6 Ultrasonic_Distance sensor"
+    //% advanced=true
+    export function sonar_ping_2(): number {
+        let d1 = 0;
+        let d2 = 0;
+
+        for (let i = 0; i < 5; i++) {
+            // send
+            basic.pause(5);
+            pins.setPull(DigitalPin.P2, PinPullMode.PullNone);
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            control.waitMicros(2);
+            pins.digitalWritePin(DigitalPin.P14, 1);
+            control.waitMicros(10);
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            // read
+            d1 = pins.pulseIn(DigitalPin.P10, PulseValue.High, 500 * 58);
+            d2 = d2 + d1;
+        }
+        return Math.round(Math.idiv(d2 / 5, 58) * 1.5);
+    }
+
+
+
+
+    //% color="#009A00" weight=30 block="(minimam 5cm) dstance |%limit| cm  |%nagasa| " group="6 Ultrasonic_Distance sensor"
+    //% limit.min=5 limit.max=30 limit.defl=5
+    //% advanced=true
+    export function sonar_ping_3(limit: number, nagasa: kyori): boolean {
+        let d1 = 0;
+        let d2 = 0;
+        if (limit < 8) {
+            limit = 8
+        }
+        for (let i = 0; i < 5; i++) {
+            // send
+            basic.pause(5);
+            pins.setPull(DigitalPin.P2, PinPullMode.PullNone);
+            pins.digitalWritePin(DigitalPin.P1, 0);
+            control.waitMicros(2);
+            pins.digitalWritePin(DigitalPin.P14, 1);
+            control.waitMicros(10);
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            // read
+            d1 = pins.pulseIn(DigitalPin.P10, PulseValue.High, 500 * 58);
+            d2 = d1 + d2;
+        }
+        switch (nagasa) {
+            case kyori.short:
+                if (Math.idiv(d2 / 5, 58) * 1.5 < limit) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case kyori.long:
+                if (Math.idiv(d2 / 5, 58) * 1.5 < limit) {
+                    return false;
+                } else {
+                    return true;
+                }
+                break;
         }
     }
-    if (走行モード == 2) {
-        if (左カウンター < 右カウンター) {
-            pins.analogWritePin(AnalogPin.P13, 1023)
-            pins.analogWritePin(AnalogPin.P15, 450)
-        } else {
-            pins.analogWritePin(AnalogPin.P13, 450)
-            pins.analogWritePin(AnalogPin.P15, 1023)
+
+
+    //% color="#f071bd" weight=30 blockId=auto_photo_R block="right_photoreflector" group="7 photoreflector"
+    //% advanced=true
+    export function phto_R() {
+        return pins.digitalReadPin(DigitalPin.P4);
+    }
+
+    //% color="#f071bd" weight=28 blockId=auto_photo_L block="left_photoreflector" group="7 photoreflector"
+    //% advanced=true
+    export function phto_L() {
+        return pins.digitalReadPin(DigitalPin.P3);
+    }
+
+
+    //% color="#6041f1"  weight=33 block="only right |%wb| " group="7 photoreflector"
+    //% sence.min=10 sence.max=40
+    //% advanced=true
+    export function photo_R_out(wb: whiteblack): boolean {
+
+        switch (wb) {
+            case whiteblack.black:
+                if ((pins.digitalReadPin(DigitalPin.P3) == 1) && (pins.digitalReadPin(DigitalPin.P4) == 0)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case whiteblack.white:
+                if ((pins.digitalReadPin(DigitalPin.P3) == 0) && (pins.digitalReadPin(DigitalPin.P4) == 1)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
         }
     }
-    serial.writeNumbers([左カウンター, 右カウンター])
-})
+
+    //% color="#6041f1"  weight=34 block="onle left |%wb|" group="7 photoreflector" 
+    //% advanced=true
+    export function photo_L_out(wb: whiteblack): boolean {
+
+
+        switch (wb) {
+            case whiteblack.black:
+                if
+
+                    ((pins.digitalReadPin(DigitalPin.P3) == 0) && (pins.digitalReadPin(DigitalPin.P4) == 1)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case whiteblack.white:
+                if ((pins.digitalReadPin(DigitalPin.P3) == 1) && (pins.digitalReadPin(DigitalPin.P4) == 0)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+        }
+    }
+    //% color="#6041f1"  weight=35 block="Both |%wb| " group="7 photoreflector"
+    //% advanced=true
+    export function photo_LR_out(wb: whiteblack): boolean {
+
+        switch (wb) {
+            case whiteblack.black:
+                if
+                    ((pins.digitalReadPin(DigitalPin.P3) == 0) && (pins.digitalReadPin(DigitalPin.P4) == 0)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+
+            case whiteblack.white:
+
+                if
+                    ((pins.digitalReadPin(DigitalPin.P3) == 1) && (pins.digitalReadPin(DigitalPin.P4) == 1)) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+        }
+
+    }
+
+    /*
+        smbus.writeByte(0x81, 0x00)  //0x81=10000001  RGB timing 700ms
+        smbus.writeByte(0x81, 0x10)  //16×gain
+    
+        smbus.writeByte(0x80, 0x03)  //0x03を書くと動作開始
+        smbus.writeByte(0x81, 0x2b)  //this.addr 0x29 0x81=10000001 0x2b=00101011
+    */
+    smbus.writeByte(0x81, 0xF6)  //cycle10
+
+
+    smbus.writeByte(0x80, 0x03)  //0x03を書くと動作開始
+
+
+
+
+
+
+
+
+
+
+    //% color="#ffa500"  weight=35 block="values light" group="8 color senser"
+    //% advanced=true
+    export function getLight(): number {
+
+        let result: Buffer = smbus.readBuffer(0xb4, pins.sizeOf(NumberFormat.UInt16LE) * 4)
+
+        return smbus.unpack("HHHH", result)[0]
+    }
+
+    //% color="#ffa500"  weight=35 block="values red" group="8 color senser"
+    //% advanced=true
+    export function getRed(): number {
+        return Math.round(rgb()[0])
+    }
+
+
+
+    //% color="#ffa500"  weight=35 block="values green" group="8 color senser"
+    //% advanced=true
+    export function getGreen(): number {
+        return Math.round(rgb()[1])
+    }
+
+
+
+
+    //% color="#ffa500"  weight=35 block="values blue" group="8 color senser"
+    //% advanced=true
+    export function getBlue(): number {
+        return Math.round(rgb()[2])
+    }
+
+
+
+
+
+
+    export function rgb(): number[] {
+        let result: number[] = raw()
+        let clear: number = result.shift()
+        for (let x: number = 0; x < result.length; x++) {
+            result[x] = result[x] * 255 / clear
+        }
+        return result
+    }
+
+
+    export function raw(): number[] {
+
+        let result: Buffer = smbus.readBuffer(0xb4, pins.sizeOf(NumberFormat.UInt16LE) * 4)
+        return smbus.unpack("HHHH", result)
+    }
+
+
+    //% color="#ffa500"  weight=16 blockId=color_temp block="color Temperatures value" group="8 color senser"
+    //% advanced=true
+    export function color_temp(): number {
+        return Math.round(3810 * getBlue() / getRed() + 1391)
+    }
+
+
+    //% color="#ffa500" weight=88 blockId=selectcycle
+    //% block="choice |%cycle|" group="8 color senser"
+    //% advanced=true
+    export function selectcycle(cycle: colorcycle): void {
+
+        switch (cycle) {
+            case colorcycle.cycle1:
+                smbus.writeByte(0x81, 0xFF)
+                break;
+            case colorcycle.cycle10:
+                smbus.writeByte(0x81, 0xF6)
+                break;
+            case colorcycle.cycle42:
+                smbus.writeByte(0x81, 0xD5)
+                break;
+            case colorcycle.cycle64:
+                smbus.writeByte(0x81, 0xC0)
+                break;
+            case colorcycle.cycle256:
+                smbus.writeByte(0x81, 0x00)
+                break;
+        }
+    }
+
+    /*
+        //% color="#ffa500" weight=88 blockId=selectgain
+        //% block="choice |%gain|" group="8 color senser"
+        //% advanced=true
+        export function selectgain(gain: colorgain): void {
+    
+            switch (gain) {
+                case colorgain.gain1:
+                    smbus.writeByte(0x8C, 0x00)
+                    break;
+                case colorgain.gain4:
+                    smbus.writeByte(0x8C, 0x01)
+                    break;
+                case colorgain.gain16:
+                    smbus.writeByte(0x8C, 0x10)
+                    break;
+                case colorgain.gain60:
+                    smbus.writeByte(0x8C, 0x11)
+                    break;
+    
+            }
+        }
+    */
+
+    //% color="#ffa500"  weight=16 blockId=color_ID block="color ID" group="8 color senser"
+    //% advanced=true
+    export function color_ID(): number {
+        color_value = 0
+        /*     黒:0　　赤：1　緑：2　青：3　茶色：4　白:7  */
+        neo_sinamon.showColor(neopixel.colors(NeoPixelColors.Black))
+
+
+        if ((getLight() > 100) && (getLight() < 400)) {
+            if ((color_temp() > 1500) && (color_temp() < 4500)) {
+                color_value = 1
+                neo_sinamon.showColor(neopixel.colors(NeoPixelColors.Red))
+            }
+
+            if ((color_temp() > 4500) && (color_temp() < 5800)) {
+                color_value = 2
+                neo_sinamon.showColor(neopixel.colors(NeoPixelColors.Green))
+            }
+
+            if ((color_temp() > 5800)) {
+                color_value = 3
+                neo_sinamon.showColor(neopixel.colors(NeoPixelColors.Blue))
+            }
+        }
+        else {
+            if (getLight() < 50) {
+                color_value = 0
+                neo_sinamon.showColor(neopixel.colors(NeoPixelColors.Black))
+            }
+            if ((getLight() > 50) && (getLight() < 100)) {
+                color_value = 4
+                neo_sinamon.showColor(neopixel.colors(NeoPixelColors.Orange))
+            }
+            if (getLight() > 400) {
+                color_value = 7
+                neo_sinamon.showColor(neopixel.colors(NeoPixelColors.White))
+            }
+
+
+
+        }
+        return color_value
+    }
+
+
+
+
+    namespace smbus {
+        export function writeByte(register: number, value: number): void {
+            let temp = pins.createBuffer(2);
+            temp[0] = register;
+            temp[1] = value;
+            pins.i2cWriteBuffer(0x29, temp, false);
+        }
+
+
+        export function readBuffer(register: number, len: number): Buffer {
+            let temp = pins.createBuffer(1);
+            temp[0] = register;
+            pins.i2cWriteBuffer(0x29, temp, false);
+            return pins.i2cReadBuffer(0x29, len, false);
+        }
+
+
+        export function unpack(fmt: string, buf: Buffer): number[] {
+            let le: boolean = true;
+            let offset: number = 0;
+            let result: number[] = [];
+            let num_format: NumberFormat = 0;
+            for (let c = 0; c < fmt.length; c++) {
+                switch (fmt.charAt(c)) {
+                    case '<':
+                        le = true;
+                        continue;
+                    case '>':
+                        le = false;
+                        continue;
+                    case 'c':
+                    case 'B':
+                        num_format = le ? NumberFormat.UInt8LE : NumberFormat.UInt8BE; break;
+                    case 'b':
+                        num_format = le ? NumberFormat.Int8LE : NumberFormat.Int8BE; break;
+                    case 'H':
+                        num_format = le ? NumberFormat.UInt16LE : NumberFormat.UInt16BE; break;
+                    case 'h':
+                        num_format = le ? NumberFormat.Int16LE : NumberFormat.Int16BE; break;
+                }
+                result.push(buf.getNumber(num_format, offset));
+                offset += pins.sizeOf(num_format);
+            }
+            return result;
+        }
+    }
+
+    basic.forever(function () {
+        if (走行モード == 1) {
+            if (左カウンター < 右カウンター) {
+                pins.analogWritePin(AnalogPin.P2, 1023)
+                pins.analogWritePin(AnalogPin.P16, 450)
+            } else {
+                pins.analogWritePin(AnalogPin.P2, 450)
+                pins.analogWritePin(AnalogPin.P16, 1023)
+            }
+        }
+        if (走行モード == 2) {
+            if (左カウンター < 右カウンター) {
+                pins.analogWritePin(AnalogPin.P13, 1023)
+                pins.analogWritePin(AnalogPin.P15, 450)
+            } else {
+                pins.analogWritePin(AnalogPin.P13, 450)
+                pins.analogWritePin(AnalogPin.P15, 1023)
+            }
+        }
+        serial.writeNumbers([左カウンター, 右カウンター])
+    })
+
+
+
+
+
 }
